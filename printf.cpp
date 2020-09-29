1f86a9c3cca0851e055f3f050108551e6d650333
Jongkeun Kim <kjkandrea@gmail.com>
2020-09-29 11:02:00 +0900

    printf

---
+++ printf.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  printf("%d + %d = %d\n", 2, 3, 2 + 3); // 정수
+  printf("저는 %s 에요. 저는 %s에 살아요\n", "강아지", "서울"); // 문자열
+  printf("%c %c %c\n", 'd', 'o', 'g'); // 문자
+  printf("%.2f\n", 3.33333); // 실수
+  printf("%.2g\n", 3.33333); // 실수 (지수)
+}
