bf307ba5617b83c07ecc7d21e8526f513279a258
Jongkeun Kim <kjkandrea@gmail.com>
2020-09-29 11:13:01 +0900

    variable

---
+++ 03-variable.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int a = 3; // 정수
+  int b = 10;
+  printf("%d\n", a);
+  printf("%d\n", a + b);
+  
+  a = 5;
+  printf("%d\n", a);
+  int sum = a + b;
+  printf("%d\n", sum);
+}
