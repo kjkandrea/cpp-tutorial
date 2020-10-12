bd07c8ad43b95fb843727ccc0ff77e286c4cc61a
Jongkeun Kim <kjkandrea@gmail.com>
2020-10-12 22:45:20 +0900

    PointerArrayDecalogue add

---
+++ pointerArrayDecalogue.cpp
@@ -0,0 +1,20 @@
+/*
+배열 포인터 삼계명 👩‍⚖️
+
+1. ptr == &ptr[0]
+2. *ptr == ptr[0]
+3. ptr + 1 == ptr + sizeof(*ptr)을 더한 값
+*/
+#include <stdio.h>
+
+int main() {
+  int arr[3] = { 1, 2, 3 };
+
+  printf("arr + 1 = %p\n", arr + 1);
+  printf("sizeof(arr) = %lu\n", sizeof(arr));
+
+  printf("&arr = %p\n", &arr);
+  printf("&arr + 1 = %p\n", &arr + 1);
+  printf("sizeof(arr) = %lu\n", sizeof(arr)); // 아하 이래서 12가 더해졌구나! 😻
+}
+
